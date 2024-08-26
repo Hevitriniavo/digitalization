65c47709a184eef4a008dc0d09396a0385863ac4
Ngitangita <hei.elise.3@gmail.com>
2024-08-26 12:35:09 +0300

    init web

---
+++ restaurant-digitalization-web/src/App.jsx
@@ -0,0 +1,13 @@
+import { Container, Typography } from '@mui/material';
+
+const App = () => {
+  return (
+    <Container>
+      <Typography variant="h1" align="center" gutterBottom>
+        Hello World
+      </Typography>
+    </Container>
+  );
+}
+
+export default App;
